ec2f1f706aaff2992db606a24170821d32ec5e38
Alumno <alumno@localhost.localdomain>
2014-02-25 09:41:59 -0600

    Tarea3

--- Herencia/src/com/me/mygdxgame/MyGdxGame.java
+++ Herencia/src/com/me/mygdxgame/MyGdxGame.java
@@ -16,15 +16,14 @@ public class MyGdxGame implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
-	private Sprite sprite2;
+	
 	private Sprite sprite;
-	private Texture texture2;
+
 	private Sprite sprite3;
 	private Texture texture3;
 	private Sprite sprite4;
 	private Texture texture4;
-	int rotacion=0;
-	float translate;
+	
 	
 	@Override
 	public void create() {		
@@ -44,20 +43,14 @@ public class MyGdxGame implements ApplicationListener {
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 		
-		texture2=new Texture(Gdx.files.internal("data/enemigo.png"));
-		sprite2=new Sprite(texture2,128,128);
-		sprite2.setPosition(-0.2f, -0.2f);
-		sprite2.setSize(0.5f,0.5f);
-		sprite2.setOrigin(sprite2.getWidth()/2, sprite2.getHeight()/2);
-		
 		texture3=new Texture(Gdx.files.internal("data/fondo.png"));
 		sprite3=new Sprite(texture3,128,64);
-		sprite3.setPosition(-0.1f, -0.1f);
-		sprite3.setSize(0.5f,0.5f);
+		sprite3.setPosition(-0.3f, -0.3f);
+		sprite3.setSize(0.7f,0.7f);
 		
 		texture4=new Texture(Gdx.files.internal("data/nave.png"));
 		sprite4=new Sprite(texture4,128,64);
-		sprite4.setPosition(-0.5f, -0.1f);
+		sprite4.setPosition(-0.01f, -0.01f);
 		sprite4.setSize(0.3f,0.3f);
 	}
 
@@ -74,20 +67,15 @@ public class MyGdxGame implements ApplicationListener {
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite2.setX(translate);
+		
 
 		sprite3.draw(batch);
 		sprite4.draw(batch);
-		sprite2.draw(batch);
-		batch.end();
-		sprite2.setRotation(rotacion);
-		rotacion+=0.1f;
-		if(Gdx.input.isTouched())
-		{
-		translate-=0.01;
-		rotacion+=20;
 		
-		}
+		batch.end();
+		
+		
+		
 	}
 
 	@Override
